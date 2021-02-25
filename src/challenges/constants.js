894140b315ebcecc5023d10a587015775e5242fe
Jeffdude <jeffmilling@gmail.com>
2021-02-25 18:19:51 +0000

    clarify location in application

--- src/challenges/constants.js
+++ src/challenges/constants.js
@@ -9,12 +9,18 @@ exports.ambassadorApplicationData = {
   shortDescription: "Apply to become an Ambassador for JMKRIDE",
   longDescription: (
     "Welcome! This is the application form to become a brand ambassador for JMKRIDE. " +
+    "At this early time of the program, we are only approving ambassadors in the USA. " +
     "Please fill out all the information below, then click submit. " +
     "Reach out to us at jmkride.com/contact-us or via our social media inbox if you " +
     "have any questions or concerns. Thank you for being an awesome member of our community!"
   ),
   award: 50,
   structure: [
+    {
+      title: "Where do you live? (Just your city, state, and country)",
+      fieldType: "TEXT_SHORT",
+      required: true,
+    },
     {
       title: "When did you start freeskating?",
       fieldType: "YEAR",
