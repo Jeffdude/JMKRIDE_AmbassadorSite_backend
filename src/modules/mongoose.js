089e63c574eca0f7ae31a5c1c3e06bc5e04ae889
Jeffdude <jeffmilling@gmail.com>
2021-08-16 19:28:54 +0000

    deploy stocktracker

--- src/modules/mongoose.js
+++ src/modules/mongoose.js
@@ -12,7 +12,7 @@ const { logInfo, logError } = require('../modules/errors.js');
 let count = 0;
 
 const getMongoDBUrl = () => {
-  if (processMode === "stocktracker" || ["development", "unittest"].includes(operationMode)) {
+  if (["development", "unittest"].includes(operationMode)) {
     return db_url[processMode][operationMode]
   }
   return ( 
