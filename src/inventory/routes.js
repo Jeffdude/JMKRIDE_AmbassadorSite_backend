111d4598f67287882031a3c785c4c59d6412e514
Jeffdude <jeffmilling@gmail.com>
2021-10-06 18:40:26 +0000

    cs-logs route

--- src/inventory/routes.js
+++ src/inventory/routes.js
@@ -186,6 +186,16 @@ exports.configRoutes = (app) => {
     PermissionMiddleware.minimumPermissionLevelRequired(PERMISSION_LEVELS.USER),
     InventoryController.getAllLogs
   ]);
+  app.get('/api/v2/logs/completeset/id/:completeSetId/inventory/id/:inventoryId', [
+    ValidationMiddleware.validJWTNeeded,
+    PermissionMiddleware.minimumPermissionLevelRequired(PERMISSION_LEVELS.USER),
+    InventoryController.getLogsByCompleteSet
+  ]);
+  app.post('/api/v2/logs/debug', [
+    ValidationMiddleware.validJWTNeeded,
+    PermissionMiddleware.minimumPermissionLevelRequired(PERMISSION_LEVELS.USER),
+    InventoryController.debug
+  ])
 
   /* CompleteSet Routes */
 
