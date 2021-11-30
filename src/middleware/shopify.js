70d4459f7cf4f76e7f8c6c3285152a8af3285887
Jeffdude <jeffmilling@gmail.com>
2021-11-30 19:35:59 +0000

    better check for shopify secrets

--- src/middleware/shopify.js
+++ src/middleware/shopify.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 
-const { logInfo, logError } = require('../modules/errors.js');
+const { logError } = require('../modules/errors.js');
 const { shopifyAppSecrets } = require('../environment.js');
 
 exports.reformatBody = (formatObj) => (req, res, next) => {
@@ -14,19 +14,20 @@ exports.reformatBody = (formatObj) => (req, res, next) => {
 }
 
 exports.validShopifyHmac = async (req, res, next) => {
-  if(!shopifyAppSecrets) {
-    let error = '[403][validShopifyHmac] Rejecting shopifyAPI request No SHOPIFY_APP_SECRETS found.'
-    logError(error)
-    return res.status(403).send({error});
-  }
   let requestHmac = req.get('X-Shopify-Hmac-Sha256');
   let requestStore = req.get('X-Shopify-Shop-Domain');
   let rawBody = req.rawBody;
+  console.log({requestHmac, requestStore, shopifyAppSecrets})
+  if(!shopifyAppSecrets || !shopifyAppSecrets[requestStore]) {
+    let error = '[403][validShopifyHmac] Rejecting shopifyAPI request. No valid SHOPIFY_APP_SECRETS found.'
+    logError(error)
+    return res.status(403).send({error});
+  }
   const generatedHmac = crypto.createHmac('sha256', shopifyAppSecrets[requestStore]).update(rawBody).digest('base64');
   if(generatedHmac !== requestHmac) {
     let error = '[403][validShopifyHmac] Unable to verify Shopify HMAC. X-Shopify-Hmac-Sha256: ' + requestHmac;
     logError(error)
-    logError('[403][validShopifyHmac] ' + JSON.stringify({shopifyAppSecret, requestHmac, generatedHmac}));
+    logError('[403][validShopifyHmac] ' + JSON.stringify({shopifyAppSecrets, requestHmac, generatedHmac}));
     return res.status(403).send({error});
   }
   next();
