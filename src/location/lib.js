ba6eb707a2432b7f516ad78eaafa6cf340770c1e
Jeffdude <jeffmilling@gmail.com>
2022-05-02 21:52:21 +0000

    fix formatting of zip codes

--- src/location/lib.js
+++ src/location/lib.js
@@ -20,16 +20,18 @@ exports.lookupLocation = async ({country, zip, extraStrings}) => {
       const result = googleResult.data.results.find(obj => obj.types.includes('postal_code'));
       if(!result) return {error: 'No results Found'}
 
-      const { long_name: country } = result.address_components.find(({types}) => types.includes('country'))
-      if(!country) return {error: 'No results Found'}
+      const { long_name: google_country } = result.address_components.find(({types}) => types.includes('country'))
+      if(!google_country) return {error: 'No results Found'}
+      const { long_name: google_zip } = result.address_components.find(({types}) => types.includes('postal_code'))
+      if(!google_zip) return {error: 'No results Found'}
 
-      if(!result.geometry.bounds && !boundsExceptionCountries.includes(country)) return {error: 'Defined Area Is Too Specific'}
+      if(!result.geometry.bounds && !boundsExceptionCountries.includes(google_country)) return {error: 'Defined Area Is Too Specific'}
 
       if(result.geometry.bounds && !isValidBoundingBox(result.geometry.bounds)) return {error: 'Defined Area Is Too Specific'}
 
       return ({
-        country,
-        zip,
+        country: google_country,
+        zip: google_zip,
         bounds: result.geometry.bounds,
         formatted_address: result.formatted_address,
         ...result.geometry.location
